d19a64fbfd6d57aeb117873c9b781fbd1f040ec7
Vladyslav <vladka@webdevelop.pro>
2018-03-24 00:10:36 -0700

    Added headers

--- app.go
+++ app.go
@@ -147,8 +147,18 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		log.Fatalf("Cannot convert data to json, %v", err)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+//	if origin := r.Header.Get("Origin"); origin != "" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-REAL")
+		w.Header().Set("Content-Type", "application/json")
+//	}
+
+	// Stop here if its Preflighted OPTIONS request
+	//if r.Method == "OPTIONS" && r.Header.Get("Accept") == "*/*" {
+	//	return
+	//}
+
 	respondWithBytes(w, code, response)
 }
 
